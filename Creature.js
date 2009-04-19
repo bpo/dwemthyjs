083ddae20bcba7b4cda36a19addc7cf101dc5b07
Brian P O'Rourke <brianorourke@gmail.com>
2009-04-18 18:48:43 -0700

    Adding basic fighting methods

--- Creature.js
+++ Creature.js
@@ -4,9 +4,9 @@
  For fun and education
  */
 
-// _initialization_ is a custom constructor method for subclassing
-function Creature(initialization) {
+function Creature(name) {
   this._traits = {};
+  this._name = name;
 
   // with no arguments, return all the traits
   // with arguments, add new traits
@@ -34,29 +34,95 @@ function Creature(initialization) {
     return f;
   }
 
+  this.hit = function( damage ) {
+    var p_up = rand( this.charisma() );
+    if( p_up % 9 == 7 ) {
+      this.life( this.life() + p_up / 4 );
+      print("[" + this._name + " magick powers up " + p_up + "!]");
+    }
+    this.life( this.life() - damage );
+    if( this.life() <= 0 ) {
+      print("[" + this._name + " has died.]");
+    }
+  }
+
+  this.fight = function( enemy, weapon ) {
+    // Mortality check
+    if( this.life() <= 0 ) {
+      print("[" + this._name + " is too dead to fight!]")
+      return this;
+    }
+
+    // Attack the opponent
+    var damage = rand( this.strength() + this.weapon() );
+    print( "[You hit with " + damage + " points of damage!]" );
+    enemy.hit( damage );
+
+    // Retaliation
+    if( enemy.life() > 0 ) {
+      var enemy_hit = rand( enemy.strength() + enemy.weapon() );
+      print( "[Your enemy hit with " + enemy_hit + " points of damage!]");
+      this.hit( enemy_hit );
+    }
+
+    return this;
+  }
+
   // default traits shared by all creatures
   this.traits("life", "strength", "charisma", "weapon");
-  // call the custom constructor if necessary
-  if(initialization) { initialization(); }
 }
 
 function Rabbit() {
-  var rabbit = new Creature();
+  var rabbit = new Creature("Rabbit");
   rabbit.traits("bombs");
   rabbit.life(10);
   rabbit.strength(2);
   rabbit.charisma(44);
   rabbit.weapon(4);
   rabbit.bombs(3);
+
+  // You could do something like rabbit["^"] to define a method with a
+  // handle of ^ like the Ruby version, but a call to it would like
+  // like rabbit["^"](enemy) - which lacks the niftyness of Ruby's
+  // rabbit ^ enemy syntax. All of the hero's attacks are written
+  // here as complete words.
+  rabbit.kick = function(enemy) { this.fight(enemy, 13); }
+  rabbit.slice = function(enemy) {
+    this.fight( enemy, rand( 4 + Math.pow((enemy.life() % 10), 2) ));
+  }
+
   return rabbit;
 }
 
-Rabbit.prototype = {
-  "^": function(enemy) { this.fight(enemy, 13); }
+
+/*Rabbit.prototype = {
+  "^": function(enemy) { this.fight(enemy, 13); },
+  yodel: function(enemy) { this.fight(enemy, 13); }
+}*/
+
+function IndustrialRaverMonkey() {
+  var monkey = new Creature("IndustrialRaverMonkey");
+  monkey.life(46);
+  monkey.strength(35);
+  monkey.charisma(91);
+  monkey.weapon(2);
+  return monkey;
 }
 
 function debug() {
   print.apply(print, arguments)
 }
 
+function puts(message) {
+  
+}
+
+function rand(n)
+{
+  return (Math.floor(Math.random()*n + 1));
+}
+
 var r = new Rabbit();
+var m = new IndustrialRaverMonkey();
+r.kick(m);
+r.slice(m);
