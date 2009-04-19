5cb11e42ef9bccd5cd7df2c08ddd93209f3c5b1e
Brian P O'Rourke <brianorourke@gmail.com>
2009-04-18 17:41:48 -0700

    Added a cheap approach for allowing constructors in subclasses, and moved all the function definitions for Creature into its base function - this allows them to be accessed in the constructor calls of   children.

--- Creature.js
+++ Creature.js
@@ -1,33 +1,58 @@
-function Creature() {
-  self = this;
-  this._traits = {};
-}
+/*
+ An attempt to port Dwemthy's Array into JavaScript
 
-Creature.prototype = {
-  traits: function() {
+ For fun and education
+ */
+
+// _initialization_ is a custom constructor method for subclassing
+function Creature(initialization) {
+  self = this;
+  self._traits = {};
+
+  // with no arguments, return all the traits
+  // with arguments, add new traits
+  self.traits = function() {
     if(arguments.length == 0) { return self._traits; }
 
     for( var i = 0; i < arguments.length; i++ ) {
       self[arguments[i]] = self.trait_function(arguments[i]);
-      debug("Argument", arguments[i]);
     }
-  },
+  }
 
-  hello_function: function() { print("hello"); },
-
-  trait_function: function(trait) {
+  // Returns a new function for a trait
+  //
+  // When called without an argument, this function will return the
+  // value of the trait.
+  // If called with an argument, it will set the trait to the
+  // appropriate value.
+  self.trait_function = function(trait) {
     f = function() {
       if(arguments.length == 0) { return this._traits[trait] };
 
       val = arguments[0];
-      debug("Function call: ", trait, "(", val, ")" );
-      if(!self._traits) { self._traits = {}; };
       this._traits[trait] = val;
     }
-    // debug("Generated trait function: ", f);
     return f;
   }
-};
+
+  // default traits shared by all creatures
+  self.traits("life", "strength", "charisma", "weapon");
+  // call the custom constructor if necessary
+  if(initialization) { initialization(); }
+}
+
+Rabbit = new Creature( function() {
+  self.traits("bombs");
+  self.life(10);
+  self.strength(2);
+  self.charisma(44);
+  self.weapon(4);
+  self.bombs(3);
+  });
+
+Rabbit.prototype = {
+
+}
 
 function debug() {
   call_f(print, arguments)
